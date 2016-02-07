aa1e46e081f99b657ae6f1da1f8ad1d053b56d68
Heather Blake <heather.blake@opencolleges.edu.au>
2016-02-08 00:35:12 +1100

    Deployment scripts

---
+++ scripts/sync.sh
@@ -0,0 +1,5 @@
+scp -i ~/Documents/shuvo/micro.pem ~/Documents/shuvo/swtch/src/dao/BaseDAO.php ubuntu@ec2-52-25-86-49.us-west-2.compute.amazonaws.com:/tmp/
+scp -i ~/Documents/shuvo/micro.pem ~/Documents/shuvo/swtch/src/dao/UserDAO.php ubuntu@ec2-52-25-86-49.us-west-2.compute.amazonaws.com:/tmp/
+scp -i ~/Documents/shuvo/micro.pem ~/Documents/shuvo/swtch/src/lib/api.php ubuntu@ec2-52-25-86-49.us-west-2.compute.amazonaws.com:/tmp/
+scp -i ~/Documents/shuvo/micro.pem ~/Documents/shuvo/swtch/conf/apache/.htaccess ubuntu@ec2-52-25-86-49.us-west-2.compute.amazonaws.com:/tmp/
+scp -i ~/Documents/shuvo/micro.pem ~/Documents/shuvo/swtch/scripts/ubuntu_setup.sh ubuntu@ec2-52-25-86-49.us-west-2.compute.amazonaws.com:/tmp/
---
+++ scripts/ubuntu_setup.sh
@@ -0,0 +1,4 @@
+cp /tmp/BaseDAO.php /var/www/api/dao/BaseDAO.php
+cp /tmp/UserDAO.php /var/www/api/dao/UserDAO.php
+cp /tmp/api.php /var/www/api/api.php
+#cp /tmp/.htaccess /var/www
